eadd11abbf44ead0b3f267a34933ec60b78f88c8
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-19 12:39:24 +0000

    notes on merging branches

---
+++ 03_version_management_with_git_basic/10_merging_branches_basics.js
@@ -0,0 +1,16 @@
+/*
+    MERGING BRANCHES - BASICS
+    ---
+        As discussed earlier, touching and working on the master branch is dangerous, so it id advisable you have a branch you will work on extensively and later merge it to your master branch
+
+        NOTE:
+            Before you do the Merging, make sure you are in the Branch where additional / other branch would be added to. So here we will make sure we are in the MASTER branch
+
+            So here we want to add our "third-branch" to the "master" so we must make sure we are currently in the "*master" before we perform the task
+
+        To MERGE, we use the following command
+            ----------------------
+            git merge third-branch
+            ----------------------
+                So here we merged the third branch to our master branch
+*/
