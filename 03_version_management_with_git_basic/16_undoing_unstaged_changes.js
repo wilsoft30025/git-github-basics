d1e9e945ce7589f379ce1072767edb9d62744af1
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-19 14:59:21 +0000

    notes for unstaged

---
+++ 03_version_management_with_git_basic/16_undoing_unstaged_changes.js
@@ -0,0 +1,13 @@
+/* 
+    UNDOING UNSTAGED CHANGES
+    ---
+     Let's say our commit is successfully done. And now you apply some changes to any of your Tracked Files like what we are typing here...
+
+     So we want to go back to the Latest Commit Status, use the ff. command
+        -------------------------------------------
+        git checkout 16_undoing_unstaged_changes.js
+        -------------------------------------------
+            since the branch we are refering to is our current branch, we use  -- or you omit it
+
+        
+*/
