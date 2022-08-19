f9ade37d53918f6a4f45b53c385b52280baf4ab1
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-19 14:39:22 +0000

    notes for deleting working directory files

--- 03_version_management_with_git_basic/15_deleting_working_directory_files.js
+++ 03_version_management_with_git_basic/15_deleting_working_directory_files.js
@@ -1,5 +1,21 @@
 /* 
-    DELETING WORKING DIRECTORY
+    DELETING WORKING DIRECTORY FILES
     ---
+        Lets assume you are currently in your preferred branch and you want to delete a specific file
+
+        First you have to check which files are currently in the "Staging Area". To see all the files us the ff. command
+            ------------
+            git ls-files
+            ------------
+
+        After Listing all the files, how can we delete it from the Working Directory...
+            -----------------------------------------
+            git rm 05_preparing_app_for_deployment.js
+            -----------------------------------------
+                After exuting the command you can use the "git status" to check the satus of your action. You can decide to "commmit" the changes
+
+                Using "git ls-files" again will prove to you that the files is no longer in the staging area...
+
+                Now finally commit it to confirm at the last stage...
     
 */
