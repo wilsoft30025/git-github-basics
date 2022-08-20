158ae5f5de46b705cf55a869c8e98a199e18fa9d
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-20 15:24:55 +0000

    notes of deeper into git

---
+++ 04_diving_deeper_into_git/01_understanding_git_stash.js
@@ -0,0 +1,66 @@
+/* 
+    UNDERSTANDING GIT STASH (git_stash)
+    ----------------------------------------------------------------
+        Let's say you have a project with latest commit and you later keep working on that project... the "stash" is like an INTERNAL MEMORY where you can save "Uncommitted" "Unstaged" changes...
+
+        Use the ff command
+            ---------
+            git stash
+            ---------
+                Let's assume you were typing something new in your file after your last commit, The moment you execute that command above, what you typed would be cleared and you will be back to your Latest Commit...
+
+                But the momemt to execute the command below
+                    ---------------
+                    git stash apply
+                    ---------------
+                        you would have your changes back with a message that "changes not staged for commit" displaying the filename which needs to be modified, where you can staged the changes and finally commit the changes if you want to...
+
+                        Stash allows you to save unstaged changes and to access these whenever you want todo so. So assuming you modified the file again, and you stash it again and again using "git stash"
+
+                        Let's assume we execute the "git stash apply" you will only see the latest modified. So if we want to see all the "stash" files change, use the ff. comand
+
+                            --------------
+                            git stash list
+                            --------------
+                                This will display the overview of all the stash changes listed. The one on top with the index 0 is always the latest stash data. 
+                                
+                                eg. overview of the lists is as follows
+                                    ----------------------------------------------
+                                    stash@{0}: WIP on master: 240234c file 1 added
+                                    stash@{1}: WIP on master: 240234c file 1 added
+                                    ----------------------------------------------
+
+                                    Now to move to or apply a specific stash use the following command by referring to the index number
+                                        -----------------
+                                        git stash apply 0
+                                        -----------------
+
+                            We can add Messages to our stash to know the exact data explaining the specific stash
+                                ------------------------------------
+                                git stash push -m "my message added"
+                                ------------------------------------
+
+                            Assuming after all the "stash" changes, you finally want to add it to your project, use the ff command. After adding successfully, it would be removed from the stash
+                                ---------------
+                                git stash pop 0
+                                ---------------
+                                    After executing the above code, you stage it i.e "git add ." and finally commit the changes
+
+
+                    DELETING A STASH
+                    ---
+                        Use the commmand below to delete stash
+                            ----------------
+                            git stash drop 0
+                            ----------------
+                                "0" is the stash index
+
+                        CLEAR ENTIRE STASH
+                        ---
+                            Use the commmand below to delete ALL stash
+                                ---------------
+                                git stash clear
+                                ---------------
+
+                                    use "git stash list" to confirme if it was cleared successfully
+*/
---
+++ 04_diving_deeper_into_git/02_bringing_lost_data_back_with_git_reflog.js
@@ -0,0 +1,52 @@
+/* 
+    Bringing Lost Data Back with Git Reflog
+    ---
+        Let say you mistakenly deleted and information, you can bring it back with Git Reflog... The Reflog stores all changes we make in our Git Project...
+
+        This is how the Git Reflog works... Let's say we have lots and commit and we used the "git reset" to delete our commit one step back, we can bring it back by using the ff command
+        
+            ----------
+            git reflog
+            ----------
+                executing the command above we see an OVERVIEW of all the changes we applied in this current branch. This does not save it forever but it's a 30days rollback storage. It contains all commits, checkouts etc... You can see that there is HASH numbers in front of every step.
+
+                Example of how git reflog displays overview
+                    -------------------------------------------------------------------
+                    06dec15 (HEAD -> master) HEAD@{0}: commit: added basic commands
+                    4c8485e HEAD@{1}: commit: updated notes
+                    7f8a949 HEAD@{2}: commit: added notes for deleting commits
+                    dd23f6a HEAD@{3}: commit: undoing staged and unstaged changes notes
+                    d1e9e94 HEAD@{4}: commit: notes for unstaged
+                    f9ade37 HEAD@{5}: checkout: moving from second-branch to master
+                    e334b65 HEAD@{6}: checkout: moving from master to second-branches
+                    -------------------------------------------------------------------
+
+                    Let's say we want to perform a hard reset, we just have to copy the hash number and use the command below
+                        ------------------------
+                        git reset --hard 4c8485e
+                        ------------------------
+                            After execution it will automatically move to that HEAD. So therefore if you initiate the the "git log" you will see that you latest HEAD is where you reset to
+
+                            So this is how you can bring back last commit into your Project...
+
+
+                NOTE:
+                    The Reflog also helps us when you DELETE BRANCHES. Let's say you have a branch and you later delete it completely from your project using the command below
+                        ---------------------------
+                        git branch -D fourth-branch
+                        ---------------------------
+                            Let's say after deleting you switched to the "* master" branch. Now go to you reflog and check exactly where you had that specific branch you delete and copy its hash number 
+                                i.e f3a76a2
+
+                            Now checkout the hash i.e
+                                --------------------
+                                git checkout f3a76a2
+                                --------------------
+
+                                Now we create a new branch which contains that commit
+                                    ---------------------
+                                    git switch -c feature
+                                    ---------------------
+
+
+*/
