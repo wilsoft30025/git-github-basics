595e676e0a2053568516f3e26fcd9e63ea3b4a9e
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-20 18:26:50 +0000

    fast forward merge example

---
+++ 04_diving_deeper_into_git/05_applying_the_fast_forward_merge.js
@@ -0,0 +1,13 @@
+/* 
+    APPLYING THE FAST-FORWARD MERGE
+    ----
+        Let's say you have a Project which is converted into a Working Directory...
+
+        Since it's a new project we already have the "* master branch" as the default branch. So Let me commit the changes from here and continue at my new branch.
+        
+        Now let's assume we create a new branch called the "feature" using the command below
+            ---------------------
+            git switch -c feature
+            ---------------------
+                
+*/
