028a69270cf04c2c63fb722fe1d7a8da34e58895
Wilfred Afagbegee <waksoftwareltd@gmail.com>
2022-08-25 12:47:10 +0000

    delete git from working directory

---
+++ 03_version_management_with_git_basic/23_delete_git_completely_from_working_directory.js
@@ -0,0 +1,11 @@
+/* 
+    DELETING GIT COMPLETELY FROM THE WORKING DIRECTORY
+    ---
+        Let's say you started a project with the "git init" and you realized that you want to completely DELETE the git from your working directory AND start again...
+
+        In you working directory execute the following command
+            -----------
+            rm -rf .git
+            -----------
+                This will permanently delete the ".git" folder. So therefore you can re-initialize your Git Again using "git init"
+*/
